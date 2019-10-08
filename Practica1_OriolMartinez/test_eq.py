71fd23e24a2bcd7f4b6a73b78d7c5519512fae57
oriol_martinez <oriolmartinez2@gmail.com>
2019-10-08 19:35:16 +0200

    .

--- Practica1_OriolMartinez/test_eq.py
+++ Practica1_OriolMartinez/test_eq.py
@@ -1,1 +1,8 @@
-from solucionadorEquacions import 
+from solucionadorEquacions import EqPrimG
+
+import unittest
+
+
+class TestEquacio(unittest.TestCase):
+    def test_eq(self):
+        self.assertEqual(EqPrimG('2x + 3 = 7').calcula().2)
